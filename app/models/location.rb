91f44d590a63dbc24da554812e801857b0082c89
Bri Halterman <halterma@gmail.com>
2024-03-11 08:11:54 -0400

    include Geocoder module in location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,16 @@
 class Location < ApplicationRecord
-  # ensure that both the IP address and the text address are present before saving the location
-  validates :ip_address, presence: true, format: { with: /\A(?:[0-9]{1,3}\.){3}[0-9]{1,3}\z/ } # alidate that the IP address is in a valid IPv4 format
+  # Ensure that both the IP address and the text address are present before saving the location
+  validates :ip_address, presence: true, format: { with: /\A(?:[0-9]{1,3}\.){3}[0-9]{1,3}\z/ } # Validate that the IP address is in a valid IPv4 format
   validates :text_address, presence: true
+
+  # Include Geocoder module
+  geocoded_by :full_address
+
+  # Callback to geocode after validation
+  after_validation :geocode, if: :address_changed?
+
+  # Define a method that returns the full address
+  def full_address
+    [street, city, state, country].compact.join(', ')
+  end
 end
